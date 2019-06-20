20ae3e75e6988dd60306b9542e467c1a77d0544d
pirzmak <szyborodziuk@gmail.com>
2019-06-20 14:11:56 +0200

    Add new article analyzator

--- newsArticleModule/getarticleapi.py
+++ newsArticleModule/getarticleapi.py
@@ -4,20 +4,20 @@ import urllib
 from bs4 import BeautifulSoup
 
 # APIKEY
-api_key = '72e7bba0059148e29591164a6be2b18b'
+API_KEY = '72e7bba0059148e29591164a6be2b18b'
 
 
 def get_data_from_news_api(from_date, to_date, query):
-    newsapi = NewsApiClient(api_key=api_key)
+    news_api = NewsApiClient(api_key=API_KEY)
 
-    response = newsapi.get_everything(q=query,
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param=from_date,
-                                      to=to_date,
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=1)
+    response = news_api.get_everything(q=query,
+                                       sources='bbc-news,the-verge',
+                                       domains='bbc.co.uk,techcrunch.com',
+                                       from_param=from_date,
+                                       to=to_date,
+                                       language='en',
+                                       sort_by='relevancy',
+                                       page=1)
 
     article_api_response = map(lambda j: ArticleApiResponse(j), response['articles'])
     full_content = map(lambda j: get_full_articles_content(j), article_api_response)
@@ -32,13 +32,13 @@ def get_full_articles_content(article):
         return parse_article(soup)
 
 
-def parse_article(soup):
+def parse_article(article):
     # kill all script and style elements
-    for script in soup(["script", "style"]):
+    for script in article(["script", "style"]):
         script.extract()  # rip it out
 
     # get text
-    text = soup.get_text()
+    text = article.get_text()
     # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
     # break multi-headlines into a line each
